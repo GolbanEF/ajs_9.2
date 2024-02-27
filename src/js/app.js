b2a27797fecf2d17a10bd04986c27f219be52929
GolbanEF <golban.ef@yandex.ru>
2024-02-27 22:14:16 +0300

    go

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import ArrayBufferConverter from './ArrayBufferConverter.js';
+
+export default function getBuffer() {
+    const data = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+    return ((input) => {
+      const buffer = new ArrayBuffer(data.length * 2);
+      const bufferView = new Uint16Array(buffer);
+      for (let i = 0; i < input.length; i += 1) {
+        bufferView[i] = input.charCodeAt(i);
+      }
+      return buffer;
+    })(data);
+  }
+  
+  const convert = new ArrayBufferConverter();
+  convert.load(getBuffer());
+  console.log(convert.toString());
